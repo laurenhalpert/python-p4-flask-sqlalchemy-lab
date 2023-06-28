3d17029c0b304c66ffd3628fb60e98ca753f26d7
Lauren <halpertln@gmail.com>
2023-06-27 19:59:51 -0600

    complete lab

--- server/app.py
+++ server/app.py
@@ -19,15 +19,24 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    this_animal = Animal.query.filter(Animal.id == id).first()
+    return f'<ul>ID: {id}</ul><ul>Name: {this_animal.name}</ul><ul>Species: {this_animal.species}</ul><ul>Zookeeper: {this_animal.zookeeper}</ul><ul>Enclosure: {this_animal.enclosure}</ul>'
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    this_zookeeper = Zookeeper.query.filter(Zookeeper.id == id).first()
+    response_body = f''
+    response_body += f'<ul>ID: {id}</ul>'
+    response_body += f'<ul>Name: {this_zookeeper.name}</ul>'
+    response_body += f'<ul>Birthday: {this_zookeeper.birthday}</ul>'
+    for animal in this_zookeeper.animals:
+        response_body += f'<ul>Animal: {animal.name}</ul>'
+    return response_body
 
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    this_enclosure = Enclosure.query.filter(Enclosure.id == id).first()
+    return f'<ul>ID: {id}</ul><ul>Environment: {this_enclosure.environment}</ul><ul>Open to Visitors: {this_enclosure.open_to_visitors}</ul>'
 
 
 if __name__ == '__main__':
